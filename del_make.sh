df25e581e485cf48b4cc9748df5d3dd4eea4dd05
cch <srdgame@gmail.com>
2009-03-02 21:27:38 +0800

    remove the .deps

--- del_make.sh
+++ del_make.sh
@@ -1,5 +1,6 @@
 find . -name 'Makefile' -exec rm -f {} \;
 find . -name 'Makefile.in' -exec rm -f {} \;
 find . -name '*.o' -exec rm -f {} \;
-find . -name '*~' -ok rm -f {} \
+find . -name '*~' -ok rm -f {} \;
+find . -type d -name '.deps' | xargs rm -fr \;
 
